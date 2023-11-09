89c3d76fd1feffba05e7bb12eda92eb6600e41d8
Paulo Boe <Pboe41@gmail.com>
2023-11-09 19:29:38 +0900

    Sound on button click

--- Assets/Sounds/ButtonSound.cs
+++ Assets/Sounds/ButtonSound.cs
@@ -3,23 +3,23 @@ using UnityEngine.UI;
 
 public class ButtonSound : MonoBehaviour
 {
-    [SerializeField] AudioClip soundClip;
+    //[SerializeField] AudioClip soundClip;
     private AudioSource audioSource;
-    private Button button;
+    //private Button button;
 
-    private void Start()
-    {
-        // Get the AudioSource component attached to the GameObject
-        audioSource = GetComponent<AudioSource>();
-        // Set the audio clip to play
-        audioSource.clip = soundClip;
+    //private void Start()
+    //{
+    //    // Get the AudioSource component attached to the GameObject
+    //    audioSource = GetComponent<AudioSource>();
+    //    // Set the audio clip to play
+    //    audioSource.clip = soundClip;
 
-        // Get the Button component
-        button = GetComponentInChildren<Button>();
+    //    // Get the Button component
+    //    button = GetComponentInChildren<Button>();
 
-        // Add a listener to the button's click event
-        button.onClick.AddListener(PlaySound);
-    }
+    //    // Add a listener to the button's click event
+    //    button.onClick.AddListener(PlaySound);
+    //}
 
     public void PlaySound()
     {
