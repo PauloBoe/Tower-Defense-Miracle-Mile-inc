f0d8db3eef201d220fbb0f7455e580199c5ace1a
AlmarvDiessen <0330841@student.rocvantwente.nl>
2023-11-13 17:27:02 +0900

    Finished Bullet particle

--- Assets/Scripts/BulletParticle.cs
+++ Assets/Scripts/BulletParticle.cs
@@ -5,9 +5,19 @@ using UnityEngine;
 public class BulletParticle : MonoBehaviour
 {
     public ParticleSystem particleSystem;
+    public Rigidbody rb;
     public int damage;
-
+    public float speed = 1f;
     List<ParticleCollisionEvent> colEvents = new List<ParticleCollisionEvent>();
+
+
+    void Start() {
+        // Get all Particle Systems attached to the GameObject
+
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * speed;
+
+    }
 
     private void OnParticleCollision(GameObject other) {
         int events = particleSystem.GetCollisionEvents(other, colEvents);
@@ -17,6 +27,9 @@ public class BulletParticle : MonoBehaviour
             health.TakeDamage(damage);
             Debug.Log(health.gameObject.name);
             particleSystem.Stop();
+            Destroy(rb.GetComponent<GameObject>());
         }
     }
+
+
 }
